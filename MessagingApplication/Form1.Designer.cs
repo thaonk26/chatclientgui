bbf7164242cb00c776082aa28e22d34798130d85
Nathan <thaonk26@uww.edu>
2016-08-31 11:11:36 -0500

    initial upload

---
+++ MessagingApplication/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace MessagingApplication
+{
+    partial class MessagingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendButton = new System.Windows.Forms.Button();
+            this.UserTextBox = new System.Windows.Forms.TextBox();
+            this.TextChat = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ButtonConnect = new System.Windows.Forms.Button();
+            this.UserNameText = new System.Windows.Forms.TextBox();
+            this.LabeluserName = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(158, 332);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(100, 25);
+            this.SendButton.TabIndex = 0;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // UserTextBox
+            // 
+            this.UserTextBox.Location = new System.Drawing.Point(20, 293);
+            this.UserTextBox.Name = "UserTextBox";
+            this.UserTextBox.Size = new System.Drawing.Size(386, 20);
+            this.UserTextBox.TabIndex = 1;
+            this.UserTextBox.TextChanged += new System.EventHandler(this.UserTextBox_TextChanged);
+            // 
+            // TextChat
+            // 
+            this.TextChat.Location = new System.Drawing.Point(20, 51);
+            this.TextChat.Multiline = true;
+            this.TextChat.Name = "TextChat";
+            this.TextChat.Size = new System.Drawing.Size(386, 209);
+            this.TextChat.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(432, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // ButtonConnect
+            // 
+            this.ButtonConnect.Location = new System.Drawing.Point(302, 29);
+            this.ButtonConnect.Name = "ButtonConnect";
+            this.ButtonConnect.Size = new System.Drawing.Size(103, 22);
+            this.ButtonConnect.TabIndex = 4;
+            this.ButtonConnect.Text = "Connect";
+            this.ButtonConnect.UseVisualStyleBackColor = true;
+            this.ButtonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // UserNameText
+            // 
+            this.UserNameText.Location = new System.Drawing.Point(82, 31);
+            this.UserNameText.Name = "UserNameText";
+            this.UserNameText.Size = new System.Drawing.Size(99, 20);
+            this.UserNameText.TabIndex = 5;
+            this.UserNameText.TextChanged += new System.EventHandler(this.UserNameText_TextChanged);
+            // 
+            // LabeluserName
+            // 
+            this.LabeluserName.AutoSize = true;
+            this.LabeluserName.Location = new System.Drawing.Point(19, 35);
+            this.LabeluserName.Name = "LabeluserName";
+            this.LabeluserName.Size = new System.Drawing.Size(57, 13);
+            this.LabeluserName.TabIndex = 6;
+            this.LabeluserName.Text = "UserName";
+            // 
+            // MessagingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 378);
+            this.Controls.Add(this.LabeluserName);
+            this.Controls.Add(this.UserNameText);
+            this.Controls.Add(this.ButtonConnect);
+            this.Controls.Add(this.TextChat);
+            this.Controls.Add(this.UserTextBox);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MessagingForm";
+            this.Text = "Nate\'s Messenger";
+            this.Load += new System.EventHandler(this.MessagingForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox UserTextBox;
+        private System.Windows.Forms.TextBox TextChat;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button ButtonConnect;
+        private System.Windows.Forms.TextBox UserNameText;
+        private System.Windows.Forms.Label LabeluserName;
+    }
+}
+
---
+++ MessagingApplication/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace MessagingApplication
+{
+    public partial class MessagingForm : Form
+    {
+        //private const int portNumber = 12000; //need observer pattern
+        //private const string hostName = "10.2.20.22";
+        delegate void SetTextCallback(string text);
+        //TcpClient client;
+        NetworkStream networkStream;
+        //Thread thread = null;
+        ////private const string hostName = "localhost";
+        //string readData = null;
+        TcpClient tcpClient = new TcpClient();
+        IPAddress ipAddress = IPAddress.Parse("10.2.20.16");
+        int PortNumber = 12000;
+        public MessagingForm()
+        {
+            InitializeComponent();
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void MessagingForm_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Connecting...");
+            try
+            {              
+                tcpClient.Connect(ipAddress, PortNumber);
+                TextChat.Text += Environment.NewLine + "Connected";
+                TextChat.Text += Environment.NewLine + "Enter the string to be transmitted";
+                networkStream = tcpClient.GetStream();
+            }
+            catch { }
+        }
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            try {
+                string str = UserTextBox.Text + "$";
+                Stream stream = tcpClient.GetStream();
+                ASCIIEncoding ascii = new ASCIIEncoding();
+                byte[] bite = ascii.GetBytes(str);
+                TextChat.Text += Environment.NewLine + "Transmitting...";
+                stream.Write(bite, 0, bite.Length);
+                byte[] bb = new byte[100];
+                //int k = stream.Read(bb, 0, 100);
+                string Response = Encoding.ASCII.GetString(bb);
+                TextChat.Text += Environment.NewLine + "Response from server: " + Response;
+                Thread thread = new Thread(GetMessage);
+                thread.Start();
+            }
+            catch { }
+            //client = new TcpClient();
+            //readData = "Client Connected to Server.";            
+            //TextMessage();
+            //client.Connect(hostName, portNumber);
+            //networkStream = client.GetStream();
+            //byte[] byteTime = Encoding.ASCII.GetBytes(readData);
+            //networkStream.Write(byteTime, 0, byteTime.Length);
+            //networkStream.Flush();
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            string s = UserNameText.Text + ": " + UserTextBox.Text;
+            networkStream = tcpClient.GetStream();
+            byte[] message = Encoding.ASCII.GetBytes(s);
+            networkStream.Write(message, 0, message.Length);
+            networkStream.Flush();
+            UserTextBox.Clear();
+        }
+        public void GetMessage()
+        {
+            networkStream = tcpClient.GetStream();
+            byte[] bytes = new byte[1024];
+            while (true)
+            {
+                try
+                {
+                int bytesRead = networkStream.Read(bytes, 0, bytes.Length);
+                this.SetText(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+                }
+                catch { Application.Exit(); }
+            }
+        }
+        private void SetText(string text)
+        {
+            if (this.UserTextBox.InvokeRequired)
+            {
+                SetTextCallback setText = new SetTextCallback(SetText);
+                this.Invoke(setText, new object[] { text });
+            }
+            else
+            {
+                this.TextChat.Text = this.TextChat.Text + "\r\n" + text;
+            }
+        }
+        private void UserTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
