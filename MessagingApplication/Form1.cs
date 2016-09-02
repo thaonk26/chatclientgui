081bcb193fa9a0a32c25854023c4f9fe08c483d0
Nathan <thaonk26@uww.edu>
2016-09-02 16:34:02 -0500

    cleaned up code

--- MessagingApplication/Form1.cs
+++ MessagingApplication/Form1.cs
@@ -16,20 +16,9 @@ namespace MessagingApplication
 {
     public partial class MessagingForm : Form
     {
-        //private const int portNumber = 12000; //need observer pattern
-        //private const string hostName = "10.2.20.22";
-        delegate void SetTextCallback(string text);
-        //TcpClient client;
-        //NetworkStream networkStream = default(NetworkStream);
-        //Thread thread = null;
-        ////private const string hostName = "localhost";
-        //string readData = null;
-        //string messageData = null;
-        //string userData = null;
-        private const int _BUFFER_SIZE = 2048;
-        private static readonly byte[] _buffer = new byte[_BUFFER_SIZE];
+        private const int bufferSize = 2048;
+        private static readonly byte[] _buffer = new byte[bufferSize];
         Socket socket;
-        TcpClient tcpClient = new TcpClient();
         IPAddress ipAddress = IPAddress.Loopback;//.Parse("192.168.0.2");
         //IPAddress ipAddress = IPAddress.Parse("10.2.20.14");
         int PortNumber = 12000;
@@ -59,12 +48,6 @@ namespace MessagingApplication
                 MessageBox.Show("Unable to Connect");
             }
         }
-        private void sendData()
-        {
-            byte[] data = Encoding.Default.GetBytes(TextChat.Text);
-            socket.Send(BitConverter.GetBytes(data.Length), 0, 4, 0);
-            socket.Send(data);
-        }
         private void ThreadMod(string mod)
         {
             TextChat.Text += Environment.NewLine + mod;
@@ -79,139 +62,58 @@ namespace MessagingApplication
                 received = current.EndReceive(result);
             }
             catch (SocketException)
-            {
-                
-                current.Close(); // Dont shutdown because the socket may be disposed and its disconnected anyway
-                
+            {               
+                current.Close(); // Dont shutdown because the socket may be disposed and its disconnected anyway                
                 return;
             }
             byte[] recBuf = new byte[received];
             Array.Copy(_buffer, recBuf, received);
             string text = Encoding.ASCII.GetString(recBuf);
             Invoke(DelegateModifyText, text);
-            current.BeginReceive(_buffer, 0, _BUFFER_SIZE, SocketFlags.None, ReceiveData, current);
+            current.BeginReceive(_buffer, 0, bufferSize, 0, ReceiveData, current);
         }
         private void ButtonConnect_Click(object sender, EventArgs e)
         {
-            //TextChat.Text += Environment.NewLine + "Connecting...";
-            //try
-            //{              
-            //    tcpClient.Connect(ipAddress, PortNumber);
-            //    TextChat.Text += Environment.NewLine + "Connected";
-            //    TextChat.Text += Environment.NewLine + "Enter the string to be transmitted";
-            //    networkStream = tcpClient.GetStream();
-            //}
-            //catch { }
             ConnectToServer();
+            ConnectionMessage();
+        }
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            SendData();
+        }      
+        private void SendData()
+        {
+            Action<string> DelegateModifyText = ThreadMod;
+            string s = UserNameText.Text + ": " + UserTextBox.Text;
+            byte[] message = Encoding.ASCII.GetBytes(s);
+            Invoke(DelegateModifyText, s);
+            socket.Send(message, 0, message.Length, 0);            
+            UserTextBox.Clear();
+        }
+        private void ConnectionMessage()
+        {
             try
             {
                 string str = "@#" + UserNameText.Text;
-                //Stream stream = tcpClient.GetStream();
                 ASCIIEncoding ascii = new ASCIIEncoding();
                 byte[] bite = ascii.GetBytes(str);
                 TextChat.Text += Environment.NewLine + "Transmitting...";
                 socket.Send(bite, 0, bite.Length, 0);
-                //stream.Write(bite, 0, bite.Length);
-                //byte[] bb = new byte[100];
-                //int k = stream.Read(bb, 0, 100);
                 string Response = Encoding.ASCII.GetString(_buffer);
                 TextChat.Text += Environment.NewLine + "Response from server: " + Response;
                 socket.Receive(_buffer, 0, _buffer.Length, 0);
-                socket.BeginReceive(_buffer, 0, _BUFFER_SIZE, SocketFlags.None, ReceiveData, socket);
-
-                Thread thread = new Thread(GetMessage);
-                thread.Start();
+                socket.BeginReceive(_buffer, 0, bufferSize, SocketFlags.None, ReceiveData, socket);
             }
             catch { }
-        }
-        //private void UpdateGUI()
-        //{
-        //    if (InvokeRequired)
-        //       Invoke(new MethodInvoker(UpdateGUI));
-        //    else if (InvokeRequired != true && userData == null)
-        //        TextChat.Text = TextChat.Text + Environment.NewLine + messageData;
-        //    else
-        //    {
-        //        TextChat.Text = TextChat.Text + Environment.NewLine + ">> " + messageData;
-                
-        //    }
-        //}
-        //private void ConnectLoop()
-        //{
-        //    int attempts = 0;
-
-        //    while (!tcpClient.Connected)
-        //    {
-        //        try
-        //        {
-        //            attempts++;
-        //            //tcpClient.Connect("10.2.20.51", 9999);
-        //            tcpClient.Connect(ipAddress, PortNumber);
-        //        }
-        //        catch (SocketException)
-        //        {
-        //            TextChat.Clear();
-        //            TextChat.AppendText("Connection attempts: " + attempts.ToString());
-        //        }
-        //    }
-        //    TextChat.Clear();
-        //    messageData = "Conected to Chat Server ...";
-        //    //UpdateGUI();
-        //    networkStream = tcpClient.GetStream();
-        //    ButtonConnect.Visible = false;
-        //    SendButton.Visible = true;
-        //    UserTextBox.Visible = true;
-
-        //    byte[] outStreamMessage = Encoding.ASCII.GetBytes(UserNameText.Text + "m$m");
-        //    networkStream.Write(outStreamMessage, 0, outStreamMessage.Length);
-        //    networkStream.Flush();
-        //    Thread ctThread = new Thread(GetMessage);
-        //    ctThread.Start();
-        //}
-        private void SendButton_Click(object sender, EventArgs e)
-        {
-            string s = UserNameText.Text + ": " + UserTextBox.Text;
-            byte[] message = Encoding.ASCII.GetBytes(s);
-            socket.Send(message, 0, message.Length, 0);
-            UserTextBox.Clear();
-        }
-        public void GetMessage()
-        {
-            byte[] bytes = new byte[1024];
-            while (tcpClient.Connected)
-            {
-                try
-                {
-                //networkStream = tcpClient.GetStream();
-                int bytesRead = socket.Receive(bytes, 0, bytes.Length, 0);
-                this.SetText(Encoding.ASCII.GetString(bytes, 0, bytesRead));
-                }
-                catch { Application.Exit(); }
-                //UpdateGUI();
-            }
-        }
-        private void SetText(string text)
-        {
-            if (this.UserTextBox.InvokeRequired)
-            {
-                SetTextCallback setText = new SetTextCallback(SetText);
-                this.Invoke(setText, new object[] { text });
-            }
-            else
-            {
-                this.TextChat.Text = this.TextChat.Text + "\r\n" + text;
-            }
         }
         private void UserTextBox_TextChanged(object sender, EventArgs e)
         {
 
         }
-
         private void UserNameText_TextChanged(object sender, EventArgs e)
         {
 
         }
-
         private void TextChat_TextChanged(object sender, EventArgs e)
         {
             TextChat.SelectionStart = TextChat.Text.Length;
